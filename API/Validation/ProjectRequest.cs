726bd371ee769614d2dd5f6ab1dad16173750ccd
Philip Miller <phil.miller84@gmail.com>
2020-07-04 20:45:24 -0700

    Added validator for ProjectRequest

---
+++ API/Validation/ProjectRequest.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace ServiceProtocol.Validation
+{
+    public class ProjectRequestValidator : AbstractValidator<ProjectRequest>
+    {
+        public ProjectRequestValidator()
+        {
+            RuleFor(i => i.Person.FirstName).NotEmpty().WithMessage("You must enter a name");
+            RuleFor(i => i.Person.FirstName).MaximumLength(25).WithMessage("Name cannot be longer than 25 characters");
+        }
+    }
+}
